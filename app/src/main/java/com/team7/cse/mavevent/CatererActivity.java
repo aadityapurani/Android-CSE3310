adb02d3c22d88426fdb3f3be6cad93008772a526
Brandon Hanner <brandon.hanner@mavs.uta.edu>
2018-03-25 18:44:28 -0500

    added pages

--- app/src/main/java/com/team7/cse/mavevent/CatererActivity.java
+++ app/src/main/java/com/team7/cse/mavevent/CatererActivity.java
@@ -1,11 +1,13 @@
 package com.team7.cse.mavevent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class CatererActivity extends AppCompatActivity {
 
@@ -13,6 +15,16 @@ public class CatererActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caterer);
-    }
 
+        // Just Testing the stuff out
+        final Button add_resources_Button = (Button) findViewById(R.id.button30);
+        add_resources_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent add_resource_Intent = new Intent(CatererActivity.this, AddResourcesActivity.class);
+                CatererActivity.this.startActivity(add_resource_Intent);
+            }
+        });
+
+    }
 }
