0f1e9d37fb8f9043f64e611154de69f75b8eb609
Jonathon <jon.kirkpatrick@sbcglobal.net>
2018-03-25 18:48:02 -0500

    added my screens

---
+++ app/src/main/java/com/team7/cse/mavevent/ViewReservedEventsActivity.java
@@ -0,0 +1,13 @@
+package com.team7.cse.mavevent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ViewReservedEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reserved_events);
+    }
+}
