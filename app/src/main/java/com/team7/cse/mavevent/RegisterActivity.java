eef5abbd837a48c957a13ff42a3b4642c40a1e15
Aaditya Purani <aaditya.purani@gmail.com>
2018-03-25 18:14:13 -0500

    Only for Debugging Activities

--- app/src/main/java/com/team7/cse/mavevent/RegisterActivity.java
+++ app/src/main/java/com/team7/cse/mavevent/RegisterActivity.java
@@ -1,11 +1,13 @@
 package com.team7.cse.mavevent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -13,6 +15,17 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // Just Testing the stuff out
+        final Button regButton = (Button) findViewById(R.id.button2);
+        regButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent catereractIntent = new Intent(RegisterActivity.this, CatererActivity.class);
+                RegisterActivity.this.startActivity(catereractIntent);
+            }
+
+        });
     }
 
 }
