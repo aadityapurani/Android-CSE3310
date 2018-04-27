ff4d5627a6b187dc4798d2811fb8f104a4d94a86
Aaditya Purani <aaditya.purani@gmail.com>
2018-04-26 19:15:21 -0500

    Schedule Activity Populated with Halls

--- app/src/main/java/com/team7/cse/mavevent/ScheduleVenueActivity.java
+++ app/src/main/java/com/team7/cse/mavevent/ScheduleVenueActivity.java
@@ -7,14 +7,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ScheduleVenueActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_venue);
+
+        listView = (ListView) findViewById(R.id.venueList);
+        String[] testArray = getResources().getStringArray(R.array.hallnames);
+        List<String> testList = Arrays.asList(testArray);
+
+        // Instanciating Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
+                android.R.layout.simple_list_item_1, testList);
+
+        // setting adapter on listview
+        listView.setAdapter(adapter);
 
 
     }
