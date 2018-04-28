630649148cc14e2c73aec8a5c01c17dbe515724f
Aaditya Purani <aaditya.purani@gmail.com>
2018-04-27 19:20:23 -0500

    OnClick added

--- app/src/main/java/com/team7/cse/mavevent/ScheduleVenueActivity.java
+++ app/src/main/java/com/team7/cse/mavevent/ScheduleVenueActivity.java
@@ -7,9 +7,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,21 +20,33 @@ public class ScheduleVenueActivity extends AppCompatActivity {
 
     ListView listView;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_venue);
 
         listView = (ListView) findViewById(R.id.venueList);
-        String[] testArray = getResources().getStringArray(R.array.hallnames);
+        final String[] testArray = getResources().getStringArray(R.array.hallnames);
         List<String> testList = Arrays.asList(testArray);
-
         // Instanciating Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
                 android.R.layout.simple_list_item_1, testList);
 
         // setting adapter on listview
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(ScheduleVenueActivity.this, testArray[position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
