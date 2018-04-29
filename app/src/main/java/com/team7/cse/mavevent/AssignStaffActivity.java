20f659e1e9d8d638ab3ad505b583b93cf321311e
Brandon Hanner <brandon.hanner@mavs.uta.edu>
2018-04-29 18:38:04 -0500

    Assign Staff almost ready for prime time

--- app/src/main/java/com/team7/cse/mavevent/AssignStaffActivity.java
+++ app/src/main/java/com/team7/cse/mavevent/AssignStaffActivity.java
@@ -2,81 +2,116 @@ package com.team7.cse.mavevent;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class AssignStaffActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class AssignStaffActivity extends AppCompatActivity  {
+    Event e;
+    ListView listview ;
+    String[] ListViewItems = new String[] {
+            "ListView ITEM-1",
+            "ListView ITEM-2",
+            "ListView ITEM-3",
+            "ListView ITEM-4",
+            "ListView ITEM-5",
+            "ListView ITEM-6",
+            "ListView ITEM-7",
+            "ListView ITEM-8",
+            "ListView ITEM-9",
+            "ListView ITEM-10"
 
-    int rowSelected;
+    };
+
+    SparseBooleanArray sparseBooleanArray ;
+
+    ArrayList<String> staff_pretty = new ArrayList<String>();
+    ArrayList<Integer> staff_id = new ArrayList<Integer>();
+
+    ArrayList<Integer> assigned_staff = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_caterer_assign_staff);
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////SPINNER LOGIC
-        Spinner assign_staff_event_spinner = (Spinner) findViewById(R.id.assign_staff_event_id);
-        assign_staff_event_spinner.setOnItemSelectedListener(this);
+        listview = (ListView)findViewById(R.id.assign_staff_list_view);
 
-        ArrayList<String> events_friendly = new ArrayList<String>();//this is the user-viewable name
-        events_friendly.add("Select Event");
-        events_friendly.add("Event A");
-        events_friendly.add("Event B");
-        events_friendly.add("Event C");
+        //////////////////////////////////////////////////////////////////////////////////populate staff_pretty and staff_id here
 
-        ArrayList<Integer> events_id = new ArrayList<Integer>();//this is the corresponding dB ID
-        events_id.add(0);
-        events_id.add(1);
-        events_id.add(2);
-        events_id.add(3);
 
-        //in reality, we need a control structure that will query the DB and for each
-        // matching query add it to the events_friendly, events_id lists (while move to next)
 
-        ArrayAdapter<String> dataEventAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, events_friendly);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (AssignStaffActivity.this,
+                        R.layout.custom_list_view,
+                         ListViewItems );
 
-        dataEventAdapter.setDropDownViewResource(R.layout.spinner_item);
 
-        assign_staff_event_spinner.setAdapter(dataEventAdapter);
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        ////////////////////////////////////////////////////////////////////////////////////////////////////SPINNER LOGIC
-        Spinner assign_staff_staff_spinner = (Spinner) findViewById(R.id.assign_staff_staff_id);
-        assign_staff_staff_spinner.setOnItemSelectedListener(this);
+        listview.setAdapter(adapter);
 
-        ArrayList<String> staff_friendly = new ArrayList<String>();//this is the user-viewable name
-        staff_friendly.add("Select Staff");
-        staff_friendly.add("Jon");
-        staff_friendly.add("Aaditya");
-        staff_friendly.add("David");
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO Auto-generated method stub
 
-        ArrayList<Integer> staff_id = new ArrayList<Integer>();//this is the corresponding dB ID
-        staff_id.add(0);
-        staff_id.add(1);
-        staff_id.add(2);
-        staff_id.add(3);
+                sparseBooleanArray = listview.getCheckedItemPositions();
 
-        //in reality, we need a control structure that will query the DB and for each
-        // matching query add it to the events_friendly, events_id lists (while move to next)
 
-        ArrayAdapter<String> dataStaffAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, staff_friendly);
+                listview.getChildAt(position).setBackgroundResource(R.drawable.rectangle_selected);
 
-        dataStaffAdapter.setDropDownViewResource(R.layout.spinner_item);
 
-        assign_staff_staff_spinner.setAdapter(dataStaffAdapter);
-        ////////////////////////////////////////////////////////////////////////////////////////////////////
+            }
+        });
+
+        // Pick a Date Button
+        final Button submit_button = (Button) findViewById(R.id.assign_staff_submit_id);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int i = 0 ;
+
+                while (i < sparseBooleanArray.size()) {
+
+                    if (sparseBooleanArray.valueAt(i))
+                    {
+                        assigned_staff.add(staff_id.get(i));
+                    }
+
+                    i++ ;
+                }
+
+                /////////////////////////////////////////////////////////////////////for loop to add staffs to assignment table
+
+                boolean wasAccepted = assignStaffdB(assigned_staff, e.getId());
+
+                if (wasAccepted)
+                {
+                    finish();
+                }
+
+                finish();
+            }
+        });
+
 
     }
-    //@Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        rowSelected = position;
 
-    }
+  public boolean  assignStaffdB(ArrayList<Integer> assigned_staff, int eventID)
+  {
+      boolean wasAccepted=false;
+      /////////////////////////for loop to assign staff for all in assigned-staff arrayList
+      return wasAccepted;
+  }
 
-    public void onNothingSelected(AdapterView<?> arg0) {
-        //TODO Auto-generated method stub
-    }
+
 }
