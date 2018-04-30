9002c750592abf28ff485660208004f92b94cef7
Jonathon <jon.kirkpatrick@sbcglobal.net>
2018-04-30 16:07:44 -0500

    Stuff 7.3

--- app/src/main/java/com/team7/cse/mavevent/ViewEventCalendarActivity.java
+++ app/src/main/java/com/team7/cse/mavevent/ViewEventCalendarActivity.java
@@ -3,6 +3,7 @@ package com.team7.cse.mavevent;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,15 +36,29 @@ public class ViewEventCalendarActivity extends AppCompatActivity {
         final DatabaseHelper db = new DatabaseHelper(ViewEventCalendarActivity.this);
 
         listView = (ListView) findViewById(R.id.vec_calendar_id);
-        pb = db.getAllEvents();
-
+        SharedPreferences mPrefs = getSharedPreferences("GLOBAL",MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("User","");
+        UserBaseModel userModel = gson.fromJson(json,UserBaseModel.class);
+        ArrayList<Event> events = null;
+        boolean isUser = true;
+        //Upload the correct data
+        if(userModel.type==0){
+            events = db.getAcceptedCatererEvents(userModel.getId());
+            isUser = false;
+        }
+        else{
+            //events = db.getUserEvents(userModel.getId());
+            events = new ArrayList<Event>();
+        }
         String[] testArray1 = new String[pb.size()];
         int i = 0;
         for (Event p : pb) {
-            testArray1[i] = p.getName();
+            testArray1[i] = p.getName() + p.getDate();
             i++;
         }
         List<String> testList = Arrays.asList(testArray1);
+
         // Instanciating Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(),
                 R.layout.custom_list_view, testList);
@@ -55,6 +72,7 @@ public class ViewEventCalendarActivity extends AppCompatActivity {
             }
         });
 
+        /*
         listView.setAdapter(adapter);
         Button cep_submit_button = (Button) findViewById(R.id.vec_exit_id);
         cep_submit_button.setOnClickListener(new View.OnClickListener() {
@@ -63,6 +81,8 @@ public class ViewEventCalendarActivity extends AppCompatActivity {
                                                      finish();
                                                  }
                                              });
+        */
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
